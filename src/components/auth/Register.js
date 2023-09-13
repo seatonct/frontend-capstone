af7ffb4e9a6cc8bcfce92dac534347618cd64c56
Charles Seaton <seatonct@gmail.com>
2023-09-13 14:56:24 -0500

    Registration/Login Code

---
+++ src/components/auth/Register.js
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+import { createUser, getUserByEmail } from "../../services/userService";
+
+export const Register = (props) => {
+  const [user, setUser] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+  });
+  let navigate = useNavigate();
+
+  const registerNewUser = () => {
+    const newUser = { ...user };
+
+    createUser(newUser).then((createdUser) => {
+      if (createdUser.hasOwnProperty("id")) {
+        localStorage.setItem(
+          "gift_user",
+          JSON.stringify({
+            id: createdUser.id,
+            // staff: createdUser.isStaff,
+          })
+        );
+
+        navigate("/");
+      }
+    });
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    getUserByEmail(user.email).then((response) => {
+      if (response.length > 0) {
+        // Duplicate email. No good.
+        window.alert("Account with that email address already exists");
+      } else {
+        // Good email, create user.
+        registerNewUser();
+      }
+    });
+  };
+
+  const updateUser = (evt) => {
+    const copy = { ...user };
+    copy[evt.target.id] = evt.target.value;
+    setUser(copy);
+  };
+
+  return (
+    <main className="auth-container">
+      <form className="auth-form" onSubmit={handleRegister}>
+        <h1 className="header">Learning Moments</h1>
+        <h2>Please Register</h2>
+        <fieldset className="auth-fieldset">
+          <div>
+            <input
+              onChange={updateUser}
+              type="text"
+              id="firstName"
+              className="auth-form-input"
+              placeholder="Enter your first name"
+              required
+              autoFocus
+            />
+          </div>
+        </fieldset>
+        <fieldset className="auth-fieldset">
+          <div>
+            <input
+              onChange={updateUser}
+              type="text"
+              id="lastName"
+              className="auth-form-input"
+              placeholder="Enter your last name"
+              required
+              autoFocus
+            />
+          </div>
+        </fieldset>
+        <fieldset className="auth-fieldset">
+          <div>
+            <input
+              onChange={updateUser}
+              type="email"
+              id="email"
+              className="auth-form-input"
+              placeholder="Email address"
+              required
+            />
+          </div>
+        </fieldset>
+        <fieldset className="auth-fieldset">
+          <div>
+            <input
+              onChange={updateUser}
+              type="password"
+              id="password"
+              className="auth-form-input"
+              placeholder="Enter a password"
+              required
+            />
+          </div>
+        </fieldset>
+        <fieldset className="auth-fieldset">
+          <div>
+            <button type="submit">Register</button>
+          </div>
+        </fieldset>
+      </form>
+    </main>
+  );
+};
