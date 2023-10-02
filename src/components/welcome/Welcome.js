27cf3267cd5ed82fcb650981c01d7ec25764c56b
Charles Seaton <seatonct@gmail.com>
2023-10-02 11:47:10 -0500

    fixed title

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -3,7 +3,7 @@ import "./Welcome.css";
 export const Welcome = () => {
   return (
     <div className="welcome-container">
-      <h1>Welcome to Gift Grabber!</h1>
+      <h1>Welcome to GiftGrabber!</h1>
       <div>
         Make someone's wishes come true <i className="fa-solid fa-gift"></i>
       </div>
