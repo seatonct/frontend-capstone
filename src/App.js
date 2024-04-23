162bbd4f548fd5b16c9142f9fa778a6851255011
Charles Seaton <seatonct@gmail.com>
2024-04-23 11:42:58 -0500

    started retrofitting userServices, Authorized, Application Views, Login, and Register

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import { Login } from "./components/auth/Login";
 import { Register } from "./components/auth/Register";
 import { Authorized } from "./views/Authorized";
 import { ApplicationViews } from "./views/ApplicationViews";
+import { Spinner } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { tryGetLoggedInUser } from "./services/userService";
 
 export const App = () => {
+  const [loggedInUser, setLoggedInUser] = useState();
+
+  useEffect(() => {
+    // user will be null if not authenticated
+    tryGetLoggedInUser().then((user) => {
+      setLoggedInUser(user);
+    });
+  }, []);
+
+  // wait to get a definite logged-in state before rendering
+  if (loggedInUser === undefined) {
+    return <Spinner />;
+  }
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
