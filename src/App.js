f3ba96dbb87780931845c8109acc77cef5a8bda4
Charles Seaton <seatonct@gmail.com>
2024-04-23 11:00:32 -0500

    added reactstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./components/auth/Login";
 import { Register } from "./components/auth/Register";
 import { Authorized } from "./views/Authorized";
 import { ApplicationViews } from "./views/ApplicationViews";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { fas } from "@fortawesome/free-solid-svg-icons";
-// import { faTrash } from "@fortawesome/free-solid-svg-icons";
-
-// library.add(fas, faTrash);
 
 export const App = () => {
   return (
